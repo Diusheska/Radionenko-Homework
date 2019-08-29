27f615541d5a342a0edd017a754539aa328a63d6
Diusheska <53336064+Diusheska@users.noreply.github.com>
2019-08-29 10:16:23 +0300

    Update path advanced.py

--- path advanced.py
+++ path advanced.py
@@ -1,8 +1,9 @@
 import os
 import time
 
+
 def check_directory():
-    full_path = input("Please type the path to your file's directory:\n").lower()
+    full_path = input("Please type the path to your file's directory:\n").upper()
     if os.path.exists(full_path):
         print("Ok, good! Such directory exists! Check out the files in it:")
         for root, dirs, files in os.walk(full_path):
@@ -12,9 +13,16 @@ def check_directory():
                 modified_date = time.ctime(os.path.getmtime(os.path.join(root, filename)))
                 print ("File name: {}, size (in bytes): {}, created date: {}, last modified date: {}".format(filename, file_size, created_date, modified_date))
     else:
-        os.mkdir(full_path) # нет проверки на дурака - рассчет на то, что юхер ввел существующий диск, слэши и тп
-        print ("Your path didn't exist, but I just created it for you - how cool it that?")
-        
-        
+        # before creating a new directory, check if disk exists and if path syntax is correct
+        drives_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        existing_drives = ['%s:' % d for d in drives_letters if os.path.exists('%s:' % d)]
+        root_drive = os.path.splitdrive(full_path)[0] # extract the first directory from the path (=drive)
+        if root_drive in existing_drives:
+            os.mkdir(full_path)
+            print ("Your path didn't exist, but I just created it for you - how cool it that?")
+        else:
+            print ("Sorry, I wanted to create a directory for you, but I need a valid drive name")
+
+
 check_directory()
 
